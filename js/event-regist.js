3c0668b6e92dd45026e54e2a5f167c3609d8c0e8
va rathana <varathana15@kit.edu.kh>
2018-07-20 14:42:39 +0700

    dd

---
+++ js/event-regist.js
@@ -0,0 +1,280 @@
+var VisionEventRegist = new Object();
+VisionEventRegist.func = new Object();
+VisionEventRegist.Var = {};
+VisionEventRegist.Var.move_scroll_bar = false;
+
+jQuery(window).ready(function(){
+    console.log("ready")
+	VisionEventRegist.func.ChangeDate( jQuery( '#VER_date_button button.active' ) );
+
+	VisionEventRegist.func.SizeSet();
+
+	/*
+	 * スクロールバーのドラッグ可能に
+	 */
+	var bars = jQuery( '.VER_bar' );
+	for( var i = 0; i < bars.length; ++i ) {
+		jQuery( bars[i] ).draggable({
+			axis : 'x' ,
+			containment : jQuery( bars[i] ).closest( '.VER_bar_wrap' ) ,
+			drag : function( event , current_info ) { VisionEventRegist.func.DragScroll( current_info ); }
+		});
+	}
+
+	/*
+	 * イベントボックスのスクロール
+	 */
+	jQuery( '.VER_event' ).on( 'scroll' , function(event) {
+		var target = event.currentTarget;
+		VisionEventRegist.func.ScrollEventBox( target );
+	});
+
+
+	/*
+	 * スクロールバーにイベント登録
+	 */
+	jQuery( '.VER_arrow' ).on( 'mousedown , touchstart' , function(event) {
+		VisionEventRegist.Var.move_scroll_bar = true;
+		var target = event.currentTarget;
+		VisionEventRegist.func.ArrowScroll( target );
+	});
+
+
+});
+
+/*
+ * スクロールバーをドラッグして動かす
+ */
+VisionEventRegist.func.DragScroll = function( current_info ) {
+	VisionEventRegist.Var.move_scroll_bar = true;
+	var bar = current_info.helper[0];
+
+	var wrapper = jQuery( bar ).closest( '.VER_scrollbar' ).find( '.VER_bar_wrap' ).get(0);
+	var wrapper_width = parseFloat( getComputedStyle( wrapper  , '').getPropertyValue( 'width' ).replace( /px$/ , '' ) );
+	wrapper_width = Math.floor( wrapper_width * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+	var bar_style = jQuery( bar ).get(0);
+	var bar_width = parseFloat( getComputedStyle( bar_style  , '').getPropertyValue( 'width' ).replace( /px$/ , '' ) );
+	bar_width = Math.floor( bar_width * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+	var max_left = wrapper_width - bar_width;
+	max_left = Math.floor( max_left * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+	var left = jQuery( current_info.position.left );
+	left = left[0];
+	if( left >= max_left ) {
+		left = max_left;
+	} else if( left === undefined ) {
+		left = 0;
+	}
+
+	var left_per = left / max_left;
+
+	var event_wrap = jQuery( bar ).closest( '.VER_wrapper' ).find( '.VER_event' );
+
+	var event_wrap_width = jQuery( event_wrap ).get(0);
+	event_wrap_width = parseFloat( getComputedStyle( event_wrap_width  , '').getPropertyValue( 'width' ).replace( /px$/ , '' ) );
+	event_wrap_width = Math.floor( event_wrap_width * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+	var event_in_location = jQuery( event_wrap ).find( '.event_in_location' ).attr( 'my_width' );
+
+	var max = event_in_location - event_wrap_width;
+	max = Math.ceil( max * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+	var scroll = max * left_per;
+	jQuery( event_wrap ).scrollLeft( scroll );
+
+	setTimeout( function(){
+		VisionEventRegist.Var.move_scroll_bar = false;
+	} , 100 );
+}
+
+/*
+ * スクロールバーの動作
+ */
+VisionEventRegist.func.ArrowScroll = function( current ) {
+	VisionEventRegist.Var.scroll_id = setInterval( function() {
+		VisionEventRegist.func.MoveScrollBar( current );
+	} , 30 );
+
+	current.onmouseup = function() {
+		clearInterval( VisionEventRegist.Var.scroll_id );
+		VisionEventRegist.Var.move_scroll_bar = false;
+	}
+	current.ontouchend = function() {
+		clearInterval( VisionEventRegist.Var.scroll_id );
+		VisionEventRegist.Var.move_scroll_bar = false;
+	}
+
+}
+
+VisionEventRegist.func.MoveScrollBar = function( current ) {
+	/*
+	 * スクロールバーを動かす
+	 */
+
+	var wrapper = jQuery( current ).closest( '.VER_scrollbar' ).find( '.VER_bar_wrap' ).get(0);
+	var wrapper_width = parseFloat( getComputedStyle( wrapper  , '').getPropertyValue( 'width' ).replace( /px$/ , '' ) );
+	wrapper_width = Math.floor( wrapper_width * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+	var bar_element = jQuery( current ).closest( '.VER_scrollbar' ).find( '.VER_bar' );
+	var bar_style = jQuery( bar_element ).get(0);
+	var bar_width = parseFloat( getComputedStyle( bar_style  , '').getPropertyValue( 'width' ).replace( /px$/ , '' ) );
+	bar_width = Math.floor( bar_width * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+	var max_left = wrapper_width - bar_width;
+	max_left = Math.floor( max_left * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+	var one_per = max_left * 0.01;
+	var now_left = jQuery( bar_element ).position().left;
+	now_left = Math.floor( now_left * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+	var direction = jQuery( current ).attr( 'direction' );
+
+
+	if( direction === 'next' ) {
+
+		if( now_left + one_per >= max_left ) {
+			to_left = max_left;
+		} else {
+			to_left = now_left + one_per;
+		}
+
+	} else if( direction === 'back' ) {
+
+		if( now_left - one_per <= 0 ) {
+			to_left = 0;
+		} else {
+			to_left = now_left - one_per;
+		}
+
+	}
+
+	jQuery( bar_element ).css( 'left' , to_left + 'px' );
+
+	/*
+	 * イベントボックスを連動させる
+	 */
+	var to_left_per = to_left / max_left;
+
+	var event_wrap = jQuery( current ).closest( '.VER_wrapper' ).find( '.VER_event' );
+
+	var event_wrap_width = jQuery( event_wrap ).get(0);
+	event_wrap_width = parseFloat( getComputedStyle( event_wrap_width  , '').getPropertyValue( 'width' ).replace( /px$/ , '' ) );
+	event_wrap_width = Math.floor( event_wrap_width * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+	var event_in_location = jQuery( event_wrap ).find( '.event_in_location' ).attr( 'my_width' );
+
+	var max = event_in_location - event_wrap_width;
+	max = Math.ceil( max * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+	var scroll = max * to_left_per;
+	jQuery( event_wrap ).scrollLeft( scroll );
+
+}
+
+/*
+ * イベントボックスのスクロール時
+ */
+VisionEventRegist.func.ScrollEventBox = function( event_wrap ) {
+	var left = jQuery( event_wrap ).scrollLeft() * -1;
+	jQuery( event_wrap ).closest( '.VER_wrapper' ).find( '.VER_time_table' ).css( 'margin-left' , left + 'px' );
+
+	/*
+	 * スクロールバーを連動させる
+	 */
+	if( ! VisionEventRegist.Var.move_scroll_bar ) {
+
+		var event_wrap_width = jQuery( event_wrap ).get(0);
+		event_wrap_width = parseFloat( getComputedStyle( event_wrap_width  , '').getPropertyValue( 'width' ).replace( /px$/ , '' ) );
+		event_wrap_width = Math.floor( event_wrap_width * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+		var event_in_location = jQuery( event_wrap ).find( '.event_in_location' ).get(0);
+		event_in_location = parseFloat( getComputedStyle( event_in_location  , '').getPropertyValue( 'width' ).replace( /px$/ , '' ) );
+		event_in_location = Math.floor( event_in_location * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+		var width = event_in_location - event_wrap_width;
+		var scroll_per = jQuery( event_wrap ).scrollLeft() / width;
+
+		var bar_wrap = jQuery( event_wrap ).closest( '.VER_wrapper' ).find( '.VER_scrollbar' ).find( '.VER_bar_wrap' ).get(0);
+
+		var bar_wrap_width = parseFloat( getComputedStyle( bar_wrap , '').getPropertyValue( 'width' ).replace( /px$/ , '' ) );
+		bar_wrap_width = Math.floor( bar_wrap_width * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+		var bar_element = jQuery( bar_wrap ).find( '.VER_bar' );
+		var bar_style = jQuery( bar_element ).get(0);
+		var bar_width = parseFloat( getComputedStyle( bar_style  , '').getPropertyValue( 'width' ).replace( /px$/ , '' ) );
+		bar_width = Math.floor( bar_width * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+
+		var max = bar_wrap_width - bar_width;
+		max = Math.floor( max * Math.pow( 10 , 2 ) ) / Math.pow( 10 , 2 );
+		jQuery( bar_element ).css( 'left' , ( max * scroll_per ) + 'px' );
+	}
+}
+
+/*
+ * スクロールバーを隠す
+ */
+VisionEventRegist.func.SizeSet = function() {
+	jQuery( 'html' ).css( 'overflow' , 'hidden' );
+	var full_width = jQuery( 'html' ).width();
+	var empty_box = jQuery( '<div></div>' , {
+		'display' : 'block' ,
+		'position' : 'absolute' ,
+		'height' : '2000px' ,
+		'width' : '300px'
+	});
+	jQuery( 'body' ).prepend( empty_box );
+	jQuery( 'html' ).css( 'overflow' , 'visible' );
+	var scroll_width = jQuery( 'html' ).width();
+
+	jQuery( empty_box ).remove();
+
+	var scrollbar_size = full_width - scroll_width;
+
+	if( scrollbar_size > 0 ) {
+		var width = jQuery( '.VER_schedule' ).width() + scrollbar_size;
+		jQuery( '.VER_schedule_scroll_wrapper' ).width( width );
+	}
+}
+
+VisionEventRegist.func.ChangeDate = function(obj) {
+	jQuery( '#VER_date_button button' ).removeClass( 'active' );
+	jQuery( obj ).addClass( 'active' );
+
+	jQuery( '.VER_wrapper' ).hide();
+	var target = jQuery( obj ).attr( 'data-target' );
+	target = target.replace( /\//g , '\\/' );
+	jQuery( '.VER_wrapper[date="' + target + '"]' ).show();
+}
+
+VisionEventRegist.func.OpenDetail = function(obj) {
+	$('body,.VER_schedule_scroll_wrapper').css({'overflow':"hidden"})
+	var target = jQuery( obj ).attr( 'event_id' );
+	var mask = jQuery( '.event_detail_mask[event_id="' + target + '"]' );
+	var box = jQuery( '.event_detail_box[event_id="' + target + '"]' );
+	var wrapper = jQuery( box ).closest( '.VER_wrapper' );
+
+	/*var top_position = jQuery( wrapper ).offset().top - jQuery( window ).scrollTop();
+	if( top_position <= 30 )
+		top_position = 30;
+    console.log("open")*/
+	jQuery( box ).css({
+		'top' : "10%",
+		'left' : "15%" ,
+		'width' : "70%",
+		'height' : "80%"
+	});
+
+	jQuery( mask ).fadeIn(250);
+	jQuery( box ).fadeIn(250);
+}
+
+VisionEventRegist.func.CloseDetail = function(obj) {
+	$('body, .VER_schedule_scroll_wrapper').css({'overflow':'auto'})
+	var target = jQuery( obj ).attr( 'event_id' );
+	var mask = jQuery( '.event_detail_mask[event_id="' + target + '"]' );
+	var box = jQuery( '.event_detail_box[event_id="' + target + '"]' );
+
+	jQuery( mask ).fadeOut(250);
+	jQuery( box ).fadeOut(250);
+}
---
+++ js/schedule.js
@@ -0,0 +1,342 @@
+if(!String.prototype.format) {
+    String.prototype.format = function() {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function(match, number){
+            return typeof args[number] != undefined
+                ? args[number] : match;
+        });
+    }
+}
+
+var MIRAI;
+if(!MIRAI) { MIRAI = {}; }
+if(!MIRAI.main) {MIRAI.main = {};}
+
+(function($){
+    var func = MIRAI.main;
+
+    func.fetchEventsData = function(url) {
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: 'json',
+            success: function(res) {
+                var list_location=[]
+                var response = JSON.parse(JSON.stringify(res)),
+                    timetables = response.event_dates[0].timetables,
+                    events = {
+                        eventOn15th: {},
+                        eventOn16th: {}
+                    };
+                var start_time_15=[];
+                var end_time_15 = []
+                var start_time_16=[];
+                var end_time_16 = [];
+                var borderHtml15=""
+                var borderHalfHtml15=""
+                var borderHtml16=""
+                var borderHalfHtml16=""
+                var widthLocation15 = 0;
+                var widthLocation16 = 0;
+                var count = 1;
+                var defaultHeightCard = 0
+                if($(document).width() <= 1023){
+                    defaultHeightCard = 80
+                }else{
+                    defaultHeightCard = 120
+                }
+                timetables.sort(func.sortLocation)
+                timetables.map(function(ele, index){
+                    var startTime = moment(ele.start).format('HH:mm'),
+                        endTime = moment(ele.end).format('HH:mm'),
+                        startDate = moment(ele.start).format('DD'),
+                        endDate = moment(ele.end).format('DD'),
+                        locationName = ele.location
+                    if(startDate === "15"){
+                        start_time_15.push(startTime.split(":")[0])
+                        end_time_15.push(endTime.split(":")[0])
+                        if(!events.eventOn15th.hasOwnProperty(locationName)){
+                            events.eventOn15th[locationName] =[]
+                            var locationElementHtml = func.locationTemplate.format(
+                                locationName
+                            )
+                            locationElementHtml = $.parseHTML(locationElementHtml);
+                            $("#location15").append(locationElementHtml)
+                        }
+                        ele["start"] = startTime
+                        ele["end"] = endTime
+                        events.eventOn15th[locationName].push(ele)
+                    }else{
+                        start_time_16.push(startTime.split(":")[0])
+                        end_time_16.push(endTime.split(":")[0])
+                        if(!events.eventOn16th.hasOwnProperty(locationName)){
+                            events.eventOn16th[locationName] = []
+                            var locationElementHtml = func.locationTemplate.format(
+                                locationName
+                            )
+                            locationElementHtml = $.parseHTML(locationElementHtml);
+                            $("#location16").append(locationElementHtml)
+                        }
+                        ele["start"] = startTime
+                        ele["end"] = endTime
+                        events.eventOn16th[locationName].push(ele)
+                    }
+                })
+
+                events.eventOn15th = func.sortDate(events.eventOn15th)
+                events.eventOn16th = func.sortDate(events.eventOn16th)
+                for(var i = Math.min(...start_time_15);i<=Math.max(...end_time_15);i++){
+                    widthLocation15 = widthLocation15 + 290
+                    var timelineHtml15 = func.timelineTemplate.format(i+":00")
+                    borderHtml15= borderHtml15 + func.hourBorderTemplate.format()
+                    timelineHtml15 = $.parseHTML(timelineHtml15);
+                    $("#time_line_15").append(timelineHtml15)
+                }
+                for(var i = Math.min(...start_time_16);i<Math.max(...end_time_16);i++){
+                    widthLocation16 = widthLocation16 + 290
+                    var timelineHtml16 = func.timelineTemplate.format(i+":00")
+                    borderHtml16 = borderHtml16 + func.hourBorderTemplate.format()
+
+                    timelineHtml16 = $.parseHTML(timelineHtml16);
+                    $("#time_line_16").append(timelineHtml16)
+                }
+                for(var key  in events.eventOn15th){
+                    var listCardEvent = "";
+                    var listDetailEvent =""
+                    var next = false
+                    for(var i in events.eventOn15th[key]){
+                       var timeStart = moment.duration(events.eventOn15th[key][i].start).asHours();
+                       var timeEnd = moment.duration(events.eventOn15th[key][i].end).asHours();
+                       var duration = (parseFloat(timeEnd - timeStart) * 290) -1
+                        var between = parseFloat(timeStart - parseInt(Math.min(...start_time_15)))
+                        var left =  between * 290
+
+
+
+                        var html = func.cardEventTemplate.format(
+                            left+"px",
+                            duration+"px",
+                            events.eventOn15th[key][i].start + " ~ " + events.eventOn15th[key][i].end,
+                            events.eventOn15th[key][i].name,
+                            "15"+count
+                        )
+                        html = $.parseHTML(html)
+                        if(next){
+                            $(html[1]).addClass('divide_column_next')
+                            next= false
+                        }
+
+                        if(parseInt(i)+1 < events.eventOn15th[key].length){
+                            if(timeEnd > moment.duration(events.eventOn15th[key][parseInt(i)+1].start).asHours()){
+                                $(html[1]).addClass('divide_column_first')
+                                next = true
+                            }
+
+                        }
+
+                        if(duration<100) {
+                            $(html[1].getElementsByClassName('time')).css({'padding': 2})
+                            $(html[1].getElementsByClassName('title')).css({'font-size': 10,"padding":0})
+
+                        }
+
+                        listCardEvent = listCardEvent + html[1].outerHTML
+                        listDetailEvent = listDetailEvent + func.detailEventTemplate.format(
+                            "15"+count,
+                            events.eventOn15th[key][i].start + " ~ " + events.eventOn15th[key][i].end,
+                            events.eventOn15th[key][i].location,
+                            events.eventOn15th[key][i].description,
+                            events.eventOn15th[key][i].name
+                        )
+                        count++
+
+                    }
+                    $('#in_location_15').append(" <div class='event_in_location' style='display: inline-flex;margin-left: 1px' " +
+                        "my_width='"+widthLocation15+"'>"
+                        +listCardEvent+listDetailEvent+borderHtml15+"</div>")
+                }
+                for(var key in events.eventOn16th){
+                    var listCardEvent = "";
+                    var listDetailEvent =""
+                    var listDuplicate=[]
+                    var next = false
+                    for(var i in events.eventOn16th[key]){
+                        var timeStart = moment.duration(events.eventOn16th[key][i].start).asHours();
+                        var timeEnd = moment.duration(events.eventOn16th[key][i].end).asHours();
+                        var duration = (parseFloat(timeEnd - timeStart) * 290) -1
+                        var between = parseFloat(timeStart - parseInt(Math.min(...start_time_16)))
+                        var left =  between * 290
+
+
+                            var html = func.cardEventMultipleTemplate.format(
+                            left+"px",
+                            duration+"px",
+                            events.eventOn16th[key][i].start + " ~ " + events.eventOn16th[key][i].end,
+                            events.eventOn16th[key][i].name,
+                            "16"+count
+                        )
+                        html = $.parseHTML(html)
+
+                        if(duration<100) {
+                            $(html[1].getElementsByClassName('time')).css({'padding': 2})
+                            $(html[1].getElementsByClassName('title')).css({'font-size': 10})
+
+                        }
+                        if(next){
+                            //$(html[1]).addClass('divide_column_next')
+                            next= false
+                            listDuplicate.push(html)
+                            if(timeEnd > moment.duration(events.eventOn16th[key][parseInt(i)+1].start).asHours()){
+                                //$(html[1]).addClass('divide_column_first')
+                                next = true
+
+                            }
+
+                        }
+                        else if(parseInt(i)+1 < events.eventOn16th[key].length){
+                            if(timeEnd > moment.duration(events.eventOn16th[key][parseInt(i)+1].start).asHours()){
+                                //$(html[1]).addClass('divide_column_first')
+                                next = true
+
+                                listDuplicate.push(html)
+
+                            }
+
+                        }
+                        else{
+                            listCardEvent = listCardEvent + html[1].outerHTML;
+                        }
+
+
+
+                        listDetailEvent = listDetailEvent + func.detailEventTemplate.format(
+                            "16"+count,
+                            events.eventOn16th[key][i].start + " ~ " + events.eventOn16th[key][i].end,
+                            events.eventOn16th[key][i].location,
+                            events.eventOn16th[key][i].description,
+                            events.eventOn16th[key][i].name
+                        )
+                        count++
+
+                    }
+
+                    var height = (defaultHeightCard/listDuplicate.length) -1
+                    for(var i in listDuplicate){
+                        $(listDuplicate[i][1]).css({'height':height,'margin-top':i*height})
+                        if(listDuplicate.length>1){
+                            $(listDuplicate[i][1].getElementsByClassName('divider_card')).css({'display':'inline-flex'})
+                            $(listDuplicate[i][1].getElementsByClassName('title')).css({'font-size':10,'line-height':"22px",'padding':0})
+                        }
+                        listCardEvent = listCardEvent + listDuplicate[i][1].outerHTML
+
+                    }
+
+                    $('#in_location_16').append(" <div class='event_in_location' style='display: inline-flex;margin-left: 1px'" +
+                        " my_width='"+widthLocation16+"'>"
+                        +listCardEvent+listDetailEvent+borderHtml16+"</div>")
+                }
+
+
+
+
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        })
+    }
+
+
+    func.sortLocation = function(a,b) {
+        if (a.location < b.location)
+            return -1;
+        if (a.location > b.location)
+            return 1;
+        return 0;
+    }
+     func.compare = function(a,b) {
+        if (a.start < b.start)
+            return -1;
+        if (a.start > b.start)
+            return 1;
+        return 0;
+    }
+
+    func.sortDate = function(event){
+        for(var i in event){
+            event[i].sort(func.compare);
+        }
+        return event
+    }
+
+
+
+
+    func.locationTemplate = `
+    <div class='one_location'>
+        <span>{0}</span>
+    </div>
+    `;
+    func.eventTemplate=`
+    <div class='event_box' style='{0}; width:{1};' event_id='{2}' onClick='VisionEventRegist.func.OpenDetail(this);'>
+        <p class='event_detail cat_color' style='background-color:#cddc39'></p>
+        <p class='event_detail time'>{3}</p>
+        <p class='event_detail mark'>{4}</p>
+        <p class='event_detail title'>{5}</p>
+        <p class='btn'>イベント概要・会場</p>
+    </div>
+
+    
+    `
+    func.hourBorderTemplate = `
+    <div class='half time_mat' style='width:144px;'></div><div class='hour time_mat' style='width:144px;'></div>
+    
+    `;
+
+
+    func.timelineTemplate = `
+    <div style='width:calc( 145px * 2 );'>{0}</div>
+    `;
+    func.cardEventTemplate=`
+    <div class='event_box' style='left:{0}; width:{1};' event_id='even_id_is_{4}' onClick='VisionEventRegist.func.OpenDetail(this);'>
+        <p class='event_detail cat_color' style='background-color:#5e9516'></p>
+        <p class='event_detail time'>{2}</p>
+        <p class='event_detail title title_card' style="width: {1}">{3}</p>
+    </div>
+    `
+
+
+    func.cardEventMultipleTemplate=`
+    <div class='event_box' style='left:{0}; width:{1};' event_id='even_id_is_{4}' onClick='VisionEventRegist.func.OpenDetail(this);'>
+        <p class='event_detail cat_color' style='background-color:#5e9516'></p>
+        <div class="divider_card">
+            <p class='event_detail time'>{2}</p>
+            <p class='event_detail title title_card' style="width: {1}">{3}</p>
+        </div>
+        
+    </div>
+    `
+
+
+    func.detailEventTemplate = `
+    <div event_id='even_id_is_{0}' class='event_detail_mask' onClick='VisionEventRegist.func.CloseDetail(this);'></div>
+    <div event_id='even_id_is_{0}' style="height: 450px" class='event_detail_box'>
+	<p class='event_detail cat_color' style='background-color:#cddc39;'></p>
+	 <i class="fas fa-times close-icon" event_id='even_id_is_{0}' onClick='VisionEventRegist.func.CloseDetail(this);'></i>
+	<p class='event_detail time'>{1}</p>
+	<p class='event_detail location'>{2}</p>
+	<p class='event_detail title'>{4}</p>
+	<p class='event_detail description'>{3}</p>
+</div>
+    `
+
+
+
+})(jQuery);
+
+$(document).ready(function() {
+
+    MIRAI.main.fetchEventsData('https://api.eventregist.com/v/2/timetable/get?event_uid=3b75c6deb1a72cf894781a8c5e4f0e64');
+
+
+});
